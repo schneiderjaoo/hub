46d50648e85d58c4ac9305bb1aebafee809f2f7e
João Schneider <joaoguischneiders@gmail.com>
2024-06-10 18:04:23 -0300

    começado método de reserva de Salas

--- src/main/java/com/room/hub/dto/ReservaDeSala.java
+++ src/main/java/com/room/hub/dto/ReservaDeSala.java
@@ -1,5 +1,16 @@
 package com.room.hub.dto;
 
-public class ReservaDeSala {
-    
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReservaDeSala{
+    @NotBlank
+    private String nomeSala;
+    private double valorSala;
+    private LocalDate dataReserva;
 }
