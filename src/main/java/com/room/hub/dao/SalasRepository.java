2a223a74864e1c131944edfd234a9e7b49038826
João Schneider <joaoguischneiders@gmail.com>
2024-11-19 21:19:44 -0300

    Comentário desnecessário #14

--- src/main/java/com/room/hub/dao/SalasRepository.java
+++ src/main/java/com/room/hub/dao/SalasRepository.java
@@ -7,7 +7,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SalasRepository extends CrudRepository<Salas, Long> {
-    //Gambi braba
-    //@Query("SELECT MAX(s.id) FROM Salas s")
-    //Long gambi();
 }
