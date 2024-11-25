eb77db359683eefbb4f0899c2ee6273d4d374c30
João Schneider <joaoguischneiders@gmail.com>
2024-11-25 20:11:46 -0300

    Criado um enum para nivel de usuario

---
+++ src/main/java/com/room/hub/model/NivelDeUsuario.java
@@ -0,0 +1,8 @@
+package com.room.hub.model;
+
+public enum NivelDeUsuario {
+    ADMIN,       // Usuário com permissões administrativas
+    MODERADOR,   // Usuário com permissões intermediárias
+    CLIENTE,     // Usuário final ou cliente comum
+    VISITANTE    // Usuário com acesso restrito
+}
